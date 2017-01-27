41d77ef363a5369a22fbc123b395d01a3fb537af
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-01-27 18:38:19 +0200

    Rename sample package to main

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,4 +1,4 @@
-package sample;
+package main;
 
 public class Controller {
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
