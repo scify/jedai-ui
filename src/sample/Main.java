5334f1a0d3ead7370cd77a332b0ee2aabf98b759
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-01-27 18:29:09 +0200

    Add very basic intro page with start button and title

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,10 +9,10 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Introduction.fxml"));
+        primaryStage.setTitle("JedAI");
+        primaryStage.setScene(new Scene(root, 768, 480));
         primaryStage.show();
     }
 
