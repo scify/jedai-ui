f9feada85948c396905dae7f8691d533d3b9d139
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-02-10 10:32:27 +0200

    Make step 4 be comparison refinement method selection

--- src/main/java/wizard/steps/Step4Controller.java
+++ src/main/java/wizard/steps/Step4Controller.java
@@ -1,7 +1,10 @@
 package wizard.steps;
 
 import com.google.inject.Inject;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.VBox;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +14,7 @@ import wizard.WizardData;
 
 public class Step4Controller {
     public VBox containerVBox;
+    public ComboBox<String> methodCombobox;
     private Logger log = LoggerFactory.getLogger(Step4Controller.class);
 
     @Inject
@@ -19,6 +23,22 @@ public class Step4Controller {
 
     @FXML
     public void initialize() {
+        // Add options to combobox
+        ObservableList<String> comboboxOptions =
+                FXCollections.observableArrayList(
+                        "Comparison Propagation",
+                        "Cardinality Edge Pruning (CEP)",
+                        "Cardinality Node Pruning (CNP)",
+                        "Weighed Edge Pruning (WEP)",
+                        "Weighed Node Pruning (WNP)",
+                        "Reciprocal Cardinality Node Pruning (ReCNP)",
+                        "Reciprocal Weighed Node Pruning (ReWNP)"
+                );
+
+        methodCombobox.setItems(comboboxOptions);
+
+        // Bind combobox selection to model
+        methodCombobox.valueProperty().bindBidirectional(model.comparisonRefinementMethodProperty());
     }
 
     @Validate
