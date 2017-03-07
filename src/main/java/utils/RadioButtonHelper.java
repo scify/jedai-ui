812cbae25e3da84f155eb2bf94843107ccb9e9d2
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-03-07 11:10:19 +0200

    Add helper class to create radio buttons from a list of strings

---
+++ src/main/java/utils/RadioButtonHelper.java
@@ -0,0 +1,40 @@
+package utils;
+
+import javafx.beans.property.StringProperty;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.Pane;
+import jfxtras.scene.control.ToggleGroupValue;
+
+import java.util.List;
+
+public class RadioButtonHelper {
+    /**
+     * Create a radio button for each String in the list, add them to the container, and bind the selected value to the
+     * String property
+     *
+     * @param container     Container to put radio buttons in
+     * @param radioBtns     List of Strings, one for each radio button
+     * @param modelProperty Property to bind selected radio button's value to
+     */
+    public static void createButtonGroup(Pane container, List<String> radioBtns, StringProperty modelProperty) {
+        // Create new ToggleGroup and ToggleGroupValue for the radio buttons
+        ToggleGroupValue groupValue = new ToggleGroupValue();
+        ToggleGroup btnsGroup = new ToggleGroup();
+
+        // Create a radio button for each option
+        for (String s : radioBtns) {
+            // Create radio button for this option
+            RadioButton radioBtn = new RadioButton(s);
+            radioBtn.setUserData(s);
+            radioBtn.setToggleGroup(btnsGroup);
+
+            // Add to RadioButton to the container pane and the ToggleGroupValue
+            container.getChildren().add(radioBtn);
+            groupValue.add(radioBtn, radioBtn.getUserData());
+        }
+
+        // Bind toggle group value to model
+        modelProperty.bindBidirectional(groupValue.valueProperty());
+    }
+}
--- src/main/java/wizard/steps/Step2Controller.java
+++ src/main/java/wizard/steps/Step2Controller.java
@@ -1,24 +1,22 @@
 package wizard.steps;
 
 import com.google.inject.Inject;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
-import jfxtras.scene.control.ToggleGroupValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.RadioButtonHelper;
 import wizard.Submit;
 import wizard.Validate;
 import wizard.WizardData;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Step2Controller {
     public VBox containerVBox;
     public VBox radioBtnsContainer;
-    private ToggleGroupValue radioBtnsValue;
     private Logger log = LoggerFactory.getLogger(Step2Controller.class);
 
     @Inject
@@ -27,42 +25,27 @@ public class Step2Controller {
 
     @FXML
     public void initialize() {
-        // Create ToggleGroup and ToggleGroupValue for the radio buttons
-        ToggleGroup blockBuilding = new ToggleGroup();
-        radioBtnsValue = new ToggleGroupValue<>();
-
         // Create List with options
-        ObservableList<String> options =
-                FXCollections.observableArrayList(
-                        "Standard/Token Blocking",
-                        "Attribute Clustering",
-                        "Sorted Neighborhood",
-                        "Extended Sorted Neighborhood",
-                        "Q-Grams Blocking",
-                        "Extended Q-Grams Blocking",
-                        "Suffix Arrays Blocking",
-                        "Extended Suffix Arrays Blocking"
-                );
+        List<String> options = Arrays.asList(
+                "Standard/Token Blocking",
+                "Attribute Clustering",
+                "Sorted Neighborhood",
+                "Extended Sorted Neighborhood",
+                "Q-Grams Blocking",
+                "Extended Q-Grams Blocking",
+                "Suffix Arrays Blocking",
+                "Extended Suffix Arrays Blocking"
+        );
 
-        // Create a radio button for each option
-        for (String s : options) {
-            // Create radio button for this option
-            RadioButton radioBtn = new RadioButton(s);
-            radioBtn.setUserData(s);
-            radioBtn.setToggleGroup(blockBuilding);
-
-            // Add to RadioButton to the VBox and the ToggleGroupValue
-            radioBtnsContainer.getChildren().add(radioBtn);
-            radioBtnsValue.add(radioBtn, radioBtn.getUserData());
-        }
-
-        // Bind toggle group value to model
-        model.blockBuildingProperty().bindBidirectional(radioBtnsValue.valueProperty());
+        // Create radio buttons using helper method
+        RadioButtonHelper.createButtonGroup(radioBtnsContainer, options, model.blockBuildingProperty());
     }
 
     @Validate
     public boolean validate() throws Exception {
-        if (radioBtnsValue.getValue() == null || radioBtnsValue.getValue().toString().isEmpty()) {
+        String value = model.blockBuildingProperty().getValue();
+
+        if (value == null || value.isEmpty()) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
             alert.setTitle("Block Building Selection");
             alert.setHeaderText("Missing Field");
