92e0b8a627967bd396e7169b3f0274d6ecc8bb53
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-03-09 11:45:55 +0200

    Update selection confirmation screen

--- src/main/java/wizard/steps/ConfirmController.java
+++ src/main/java/wizard/steps/ConfirmController.java
@@ -1,24 +1,29 @@
 package wizard.steps;
 
 import com.google.inject.Inject;
+import javafx.beans.value.ChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import wizard.Submit;
 import wizard.WizardData;
 
 public class ConfirmController {
-    public TextField pMatcherParamField;
-    public TextField groundTruthTextField;
     public Label profileMatcherLabel;
     public ListView<String> blockRefinementList;
+    public Label erTypeLabel;
+    public Label entityProfilesD1Label;
+    public Label entityProfilesD2Label;
+    public Label groundTruthLabel;
+    public Label blockBuildingLabel;
+    public Label compRefinementLabel;
+    public Label entityMatchingLabel;
+    public Label pMatcherParamLabel;
+    public Label entityClusteringLabel;
+    public Label entityProfilesD2TitleLabel;
     private Logger log = LoggerFactory.getLogger(ConfirmController.class);
-
-    @FXML
-    TextField entityProfilesTextField, blockBuildingTextField, compRefinementTextField, entityMatchingTextField, entityClusteringTextField;
 
     @Inject
     private
@@ -26,19 +31,42 @@ public class ConfirmController {
 
     @FXML
     public void initialize() {
-        entityProfilesTextField.textProperty().bind(model.entityProfilesPathProperty());
-        blockBuildingTextField.textProperty().bind(model.blockBuildingProperty());
-        groundTruthTextField.textProperty().bind(model.groundTruthPathProperty());
-        compRefinementTextField.textProperty().bind(model.comparisonRefinementMethodProperty());
-        entityMatchingTextField.textProperty().bind(model.entityMatchingProperty());
-        entityClusteringTextField.textProperty().bind(model.entityClusteringProperty());
-        pMatcherParamField.textProperty().bind(model.profileMatcherParamProperty());
+        erTypeLabel.textProperty().bind(model.erTypeProperty());
+        entityProfilesD1Label.textProperty().bind(model.entityProfilesPathProperty());
+        blockBuildingLabel.textProperty().bind(model.blockBuildingProperty());
+        groundTruthLabel.textProperty().bind(model.groundTruthPathProperty());
+        compRefinementLabel.textProperty().bind(model.comparisonRefinementMethodProperty());
+        entityMatchingLabel.textProperty().bind(model.entityMatchingProperty());
+        entityClusteringLabel.textProperty().bind(model.entityClusteringProperty());
 
-        // Show or hide profile matcher parameter text field, depending on if it is selected or not
-        model.entityMatchingProperty().addListener(((observable, oldValue, newValue) -> {
-            profileMatcherLabel.setVisible(model.getEntityMatching().equals("Profile Matcher"));
-            pMatcherParamField.setVisible(model.getEntityMatching().equals("Profile Matcher"));
-        }));
+        // Create listeners for profile matcher and 2nd dataset path
+        ChangeListener<String> profileMatcherListener = (observable, oldValue, newValue) -> {
+            if (model.getEntityMatching().equals("Profile Matcher")) {
+                // Profile Matcher selected, add profile matcher parameter
+                pMatcherParamLabel.setText(model.getProfileMatcherParam());
+            } else {
+                // Group Linkage selected, so prof. matcher parameter is not applicable
+                pMatcherParamLabel.setText("Not applicable");
+            }
+        };
+
+        ChangeListener<String> datasetPathListener = (observable, oldValue, newValue) -> {
+            if (model.getErType().equals("Clean-Clean ER")) {
+                // Add the new value to the text field
+                entityProfilesD2Label.setText(model.getEntityProfilesD2Path());
+            } else {
+                // Dirty ER was selected, so 2nd dataset is not applicable
+                entityProfilesD2Label.setText("Not applicable");
+            }
+        };
+
+        // Add listeners for profile matcher parameter
+        model.entityMatchingProperty().addListener(profileMatcherListener);
+        model.profileMatcherParamProperty().addListener(profileMatcherListener);
+
+        // Add listeners for 2nd dataset path
+        model.erTypeProperty().addListener(datasetPathListener);
+        model.entityProfilesD2PathProperty().addListener(datasetPathListener);
 
         // Show block refinement methods in list
         model.blockProcessingMethodsProperty().addListener(((observable, oldValue, newValue) -> {
