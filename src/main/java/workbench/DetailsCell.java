d8fedafc89233b90358bd2b715e5993fff9fef30
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-05-21 17:42:53 +0300

    Don't allow opening multiple popups for the same Workflow run

--- src/main/java/workbench/DetailsCell.java
+++ src/main/java/workbench/DetailsCell.java
@@ -13,6 +13,7 @@ import wizard.steps.CompletedController;
 import wizard.steps.ConfirmController;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,46 +23,58 @@ import java.util.List;
 public class DetailsCell<T> extends TableCell<T, Void> {
     private final Hyperlink link;
     private final List<WizardData> detailedRunData;
+    private final List<WizardData> openedPopups;
 
     public DetailsCell(List<WizardData> detailedRunData, Injector injector) {
         this.detailedRunData = detailedRunData;
+        this.openedPopups = new ArrayList<>();
 
         // Add hyperlink and click action
         link = new Hyperlink("View");
         link.setOnAction(evt -> {
             // Get run data for this button
             int index = this.getIndex();
+
             if (this.detailedRunData.size() > index) {
                 // Get the model for this run
                 WizardData data = this.detailedRunData.get(index);
 
-                // Load FXML for the popup window
-                Parent root;
-                FXMLLoader loader = new FXMLLoader(
-                        CompletedController.class.getClassLoader().getResource("wizard-fxml/steps/Confirm.fxml"),
-                        null,
-                        new JavaFXBuilderFactory(),
-                        injector::getInstance);
-                try {
-                    root = loader.load();
-                    root.getProperties().put("controller", loader.getController());
+                // Only open a new popup if another one for this data is not open
+                if (!openedPopups.contains(data)) {
+                    // Load FXML for the popup window
+                    Parent root;
+                    FXMLLoader loader = new FXMLLoader(
+                            CompletedController.class.getClassLoader().getResource("wizard-fxml/steps/Confirm.fxml"),
+                            null,
+                            new JavaFXBuilderFactory(),
+                            injector::getInstance);
+                    try {
+                        root = loader.load();
+                        root.getProperties().put("controller", loader.getController());
 
-                    // Set the controller's model to the data one
-                    T controller = loader.getController();
-                    if (controller instanceof ConfirmController) {
-                        // Set the data to the controller
-                        ConfirmController confirmController = (ConfirmController) controller;
-                        confirmController.setModel(data);
+                        // Set the controller's model to the data one
+                        T controller = loader.getController();
+                        if (controller instanceof ConfirmController) {
+                            // Set the data to the controller
+                            ConfirmController confirmController = (ConfirmController) controller;
+                            confirmController.setModel(data);
+                        }
+
+                        // Show the popup window
+                        Stage dialog = new Stage();
+                        dialog.setScene(new Scene(root));
+                        dialog.setTitle("Run #" + (index + 1) + " Detailed Configuration");
+
+                        dialog.show();
+
+                        // Add the data of this popup in the array list of opened popups
+                        openedPopups.add(data);
+
+                        // When the popup closes, remove its data from the opened list so it can be opened again
+                        dialog.setOnCloseRequest(event -> openedPopups.remove(data));
+                    } catch (IOException e) {
+                        e.printStackTrace();
                     }
-
-                    // Show the popup window
-                    Stage dialog = new Stage();
-                    dialog.setScene(new Scene(root));
-                    dialog.setTitle("Detailed configuration for run #" + (index + 1));
-
-                    dialog.show();
-                } catch (IOException e) {
-                    e.printStackTrace();
                 }
             }
         });
