092cc796ffdd63ad6f75a231fe0e0ff61be4c312
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-07-25 12:25:33 +0300

    Display message when the selected method is parameter-free
    Should probably be a regular popup or disable the configuration button
    in the future.

--- src/main/java/wizard/DynamicConfigurationController.java
+++ src/main/java/wizard/DynamicConfigurationController.java
@@ -2,11 +2,13 @@ package wizard;
 
 import com.google.inject.Inject;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import org.apache.jena.atlas.json.JsonArray;
 
 public class DynamicConfigurationController {
     public VBox configVBox;
+    public Label configureParamsLabel;
 
     @Inject
     private WizardData model;
@@ -19,5 +21,10 @@ public class DynamicConfigurationController {
 
     public void setParameters(JsonArray parameters) {
         this.parameters = parameters;
+
+        // If the method is parameter-free, display it
+        if (this.parameters.isEmpty()) {
+            configureParamsLabel.setText("This is a parameter-free method!");
+        }
     }
 }
