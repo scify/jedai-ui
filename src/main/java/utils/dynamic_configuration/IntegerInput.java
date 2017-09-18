2cb911daa1b9617b9a4925c0e705234bfd1b1fab
Leonidas Tsekouras <leotsekouras@gmail.com>
2017-09-18 10:43:14 +0300

    Check IntegerInput value on loss of focus instead of continuously

--- src/main/java/utils/dynamic_configuration/IntegerInput.java
+++ src/main/java/utils/dynamic_configuration/IntegerInput.java
@@ -35,16 +35,48 @@ public class IntegerInput extends TextField {
             max = Integer.MAX_VALUE;
         }
 
-
         // Set default value
         if (!defaultValue.equals("-")) {
             this.textProperty().setValue(defaultValue);
             parameterValues.set(index, Integer.parseInt(defaultValue));
         }
 
-        // Add change listener to restrict to numbers input only
-        this.textProperty().addListener(
-                new IntegerInputChangeListener(this, min, max, index, parameterValues)
-        );
+        // Add listener for checking the input when focus is lost
+        this.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            // Get the text field's text
+            String currStrValue = this.getText();
+
+            // Try parsing the String
+            try {
+                int intValue = Integer.parseInt(currStrValue);
+
+                // Check if the number is out of the minimum/maximum bounds
+                if (intValue < min) {
+                    // Less than minimum, set to the minimum
+                    currStrValue = "" + min;
+                    this.setText(currStrValue);
+
+                    parameterValues.set(index, min);
+                } else if (intValue > max) {
+                    // Exceeds the max, set it to the max
+                    currStrValue = "" + max;
+                    this.setText(currStrValue);
+
+                    parameterValues.set(index, max);
+                }
+
+                // Save the value
+                parameterValues.set(index, currStrValue);
+            } catch (NumberFormatException e) {
+                if (currStrValue.length() == 0) {
+                    this.setText("0");
+                    parameterValues.set(index, 0);
+                } else {
+                    // Set to maximum value
+                    this.setText("" + max);
+                    parameterValues.set(index, max);
+                }
+            }
+        });
     }
 }
