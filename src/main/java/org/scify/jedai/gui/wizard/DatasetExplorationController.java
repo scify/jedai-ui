55e77f889dd13ca4d49c3596f415e26f844d86a7
Leonidas Tsekouras <leotsekouras@gmail.com>
2018-07-18 12:41:33 +0300

    Read the dataset and get the entities for exploration

--- src/main/java/org/scify/jedai/gui/wizard/DatasetExplorationController.java
+++ src/main/java/org/scify/jedai/gui/wizard/DatasetExplorationController.java
@@ -1,6 +1,8 @@
 package org.scify.jedai.gui.wizard;
 
 import javafx.fxml.FXML;
+import org.scify.jedai.datamodel.EntityProfile;
+import org.scify.jedai.gui.utilities.DataReadingHelper;
 import org.scify.jedai.gui.utilities.JPair;
 
 import java.util.List;
@@ -13,11 +15,30 @@ public class DatasetExplorationController {
     public void initialize() {
     }
 
+    /**
+     * Show the entities in the window.
+     */
+    private void updateView() {
+        // Read dataset
+        List<EntityProfile> entities = DataReadingHelper.getEntities(this.datasetType, this.datasetParams);
+//        System.out.println(entities.get(0));
+    }
+
     public void setDatasetType(String datasetType) {
         this.datasetType = datasetType;
+
+        // Check if we can show the entities
+        if (this.datasetType != null && this.datasetParams != null) {
+            updateView();
+        }
     }
 
     public void setDatasetParams(List<JPair<String, Object>> datasetParams) {
         this.datasetParams = datasetParams;
+
+        // Check if we can show the entities
+        if (this.datasetType != null && this.datasetParams != null) {
+            updateView();
+        }
     }
 }
