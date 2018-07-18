c57e75cdaa3ad5cc9b804b203337f8e8d5ace43b
Leonidas Tsekouras <leotsekouras@gmail.com>
2018-07-18 17:06:23 +0300

    Show ListView with entity attributes

--- src/main/java/org/scify/jedai/gui/utilities/EntityProfileNode.java
+++ src/main/java/org/scify/jedai/gui/utilities/EntityProfileNode.java
@@ -1,23 +1,25 @@
 package org.scify.jedai.gui.utilities;
 
-import javafx.scene.Node;
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import org.scify.jedai.datamodel.Attribute;
 import org.scify.jedai.datamodel.EntityProfile;
 
 /**
  * Node that displays a single Entity Profile
  */
 public class EntityProfileNode extends VBox {
+    private static final Insets zeroPadding = new Insets(0, 0, 0, 0);
+    private static final Font cellFont = Font.font("Arial", 11);
     private final EntityProfile entity;
 
     public EntityProfileNode(EntityProfile entity) {
         this.entity = entity;
-
-        // Border around entity
-        this.borderProperty().setValue(new Border(new BorderStroke(Color.BLACK,
-                BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
 
         // Add HBox for entity name & URL
         HBox entityTitle = new HBox();
@@ -26,10 +28,27 @@ public class EntityProfileNode extends VBox {
                 new Label("Entity: " + this.entity.getEntityUrl())
         );
 
-        // todo: Create node for showing the entity's attributes
-        Node entityAttributes = new Label(this.entity.toString());
+        // Create node for showing the entity's attributes
+        ListView<Attribute> attrsList = new ListView<>();
+        attrsList.setFixedCellSize(15);
+        attrsList.setCellFactory(lst ->
+                new ListCell<Attribute>() {
+                    @Override
+                    protected void updateItem(Attribute item, boolean empty) {
+                        super.updateItem(item, empty);
+                        setPadding(zeroPadding);
+                        setFont(cellFont);
+
+                        if (empty) {
+                            setText(null);
+                        } else {
+                            setText(item.toString());
+                        }
+                    }
+                });
+        attrsList.getItems().addAll(this.entity.getAttributes());
 
         // Add title & attribute nodes to the entity profile node
-        this.getChildren().addAll(entityTitle, entityAttributes);
+        this.getChildren().addAll(entityTitle, attrsList);
     }
 }
