c8c3421c3468586f96846e4c97bb8cf336dee7a4
Leonidas Tsekouras <leotsekouras@gmail.com>
2018-08-02 13:07:40 +0300

    Add exploration for Dirty ER ground truth

--- src/main/java/org/scify/jedai/gui/controllers/GroundTruthExplorationController.java
+++ src/main/java/org/scify/jedai/gui/controllers/GroundTruthExplorationController.java
@@ -1,10 +1,14 @@
 package org.scify.jedai.gui.controllers;
 
+import gnu.trove.iterator.TIntIterator;
 import javafx.fxml.FXML;
 import javafx.scene.control.Pagination;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import org.scify.jedai.datamodel.EntityProfile;
 import org.scify.jedai.datamodel.EquivalenceCluster;
+import org.scify.jedai.gui.nodes.EntityProfileNode;
 
 import java.util.List;
 
@@ -43,8 +47,43 @@ public class GroundTruthExplorationController {
                     duplicates.subList(pageIndex * pageSize, (pageIndex + 1) * pageSize);
 
             for (EquivalenceCluster dup : duplicatesToShow) {
-                System.out.println(dup);
-                // todo: Add node to show duplicates
+                // Create nodes that will hold the entities (HBox into ScrollPane)
+                ScrollPane entitiesPane = new ScrollPane();
+                entitiesPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+                entitiesPane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+
+                HBox entitiesContainer = new HBox();
+
+                // Add nodes that show duplicates depending on ER type
+                if (dirtyEr) {
+                    // Iterate on the list of entities for the 1st dataset (there is no 2nd)
+                    TIntIterator iter = dup.getEntityIdsD1().iterator();
+                    while (iter.hasNext()) {
+                        // Get the entity ID
+                        int entityId = iter.next();
+
+                        // Find the entity in the dataset
+                        EntityProfile entity = entitiesD1.get(entityId);
+
+                        // Add node for this entity
+                        EntityProfileNode entityProfileNode = new EntityProfileNode(entityId, entity);
+
+                        // Add the entity node to the entities container
+                        entitiesContainer.getChildren().add(entityProfileNode);
+                    }
+                } else {
+                    //todo
+                }
+
+                // Fix sizes
+                entitiesContainer.setFillHeight(true);
+                entitiesPane.setFitToHeight(true);
+
+                // Put the HBox in the scrolling entities pane
+                entitiesPane.setContent(entitiesContainer);
+
+                // Put the entities pane to the window
+                vBox.getChildren().add(entitiesPane);
             }
 
             return vBox;
