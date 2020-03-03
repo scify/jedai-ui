3de58aa7ec23943dd3e599c02044dffe32d04f1e
Leonidas Tsekouras <leotsekouras@gmail.com>
2020-03-03 21:35:32 +0200

    Add WorkflowStep class

---
+++ src/main/java/org/scify/jedai/gui/utilities/WorkflowStep.java
@@ -0,0 +1,51 @@
+package org.scify.jedai.gui.utilities;
+
+import javafx.beans.property.StringProperty;
+
+public class WorkflowStep {
+    private final String label;
+    private final String description;
+    private final String fxmlPath;
+    private final StringProperty configProperty;
+
+    public WorkflowStep(String label, String description, String fxmlPath, StringProperty configProperty) {
+        this.label = label;
+        this.description = description;
+        this.fxmlPath = fxmlPath;
+        this.configProperty = configProperty;
+    }
+
+    public WorkflowStep(String label, String description, String fxmlPath) {
+        this.label = label;
+        this.description = description;
+        this.fxmlPath = fxmlPath;
+        this.configProperty = null;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getFxmlPath() {
+        return fxmlPath;
+    }
+
+    public boolean hasConfigProperty() {
+        return (configProperty == null);
+    }
+
+    public String getConfigProperty() {
+        if (configProperty != null)
+            return configProperty.get();
+        else
+            return null;
+    }
+
+    public StringProperty configPropertyProperty() {
+        return configProperty;
+    }
+}
